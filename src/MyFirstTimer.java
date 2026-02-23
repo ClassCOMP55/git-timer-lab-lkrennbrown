148e7a95cbd5cf6168365bb59fb290d5c24194d3
Lucas <l_krennbrown@u.pacific.edu>
2026-02-22 22:38:56 -0800

    A timer needs to be created, implement ActionListener, and be started with .start()

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,17 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
+	private Timer t;
+	
+	
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,9 +21,23 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		numTimes = 0;
+		t = new Timer(1000, this);
+		t.setInitialDelay(3000);
+		t.start();
 	}
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+		if (numTimes == 10) {
+			t.stop();
+		}
+		
 	}
 }
