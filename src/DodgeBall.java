5fadc6258072793ee4b50194328cc369e13efa6f
Lucas <l_krennbrown@u.pacific.edu>
2026-02-22 23:03:48 -0800

    enemies populated on screen regularly

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -19,6 +19,7 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	private GLabel text;
 	private Timer movement;
 	private RandomGenerator rgen;
+	private int numTimes;
 	
 	public static final int SIZE = 25;
 	public static final int SPEED = 2;
@@ -29,6 +30,7 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	
 	public void run() {
 		rgen = RandomGenerator.getInstance();
+		numTimes = 0;
 		balls = new ArrayList<GOval>();
 		enemies = new ArrayList<GRect>();
 		
@@ -41,7 +43,11 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		numTimes++;
 		moveAllBallsOnce();
+		if (numTimes % 40 == 0) {
+			addAnEnemy();
+		}
 	}
 	
 	public void mousePressed(MouseEvent e) {
